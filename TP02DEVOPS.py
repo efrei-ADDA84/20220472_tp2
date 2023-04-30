598ed4474a22d2c244d33968e0c89c88a86d2757
lizemmagg <115583430+lizemmagg@users.noreply.github.com>
2023-04-30 17:08:41 +0200

    Update TP02DEVOPS.py
    changement api key

--- TP02DEVOPS.py
+++ TP02DEVOPS.py
@@ -1,15 +1,16 @@
 from flask import Flask, request
 from TP01DEVOPS import get_weather
+import os
 
 app = Flask(__name__)
 
-@app.route('/weather', methods=['GET'])
+@app.route('/', methods=['GET'])
 def weather():
     lat = request.args.get('lat')
     lon = request.args.get('lon')
-    api_key = request.args.get('api_key')
+    api_key = os.environ.get('API_KEY')
     weather_data = get_weather(lat, lon, api_key)
     return weather_data
-if __name__ == '__main__':
-    app.run(debug=True)
 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8081)
